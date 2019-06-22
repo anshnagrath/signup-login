ed28b378816fdbb921e2bb9e4e688ea60d940fdc
Ansh nagrath <34484528+anshnagrath@users.noreply.github.com>
2019-06-22 21:02:24 +0530

    Update routes.js
    changes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,11 +4,11 @@ import ProductController from "../controllers/ProductController"
 import checktoken from '../middlewares/authenticate'
 const router = express.Router();
 router.get('/',function(req,res){res.send("welcome to api")})
-router.post("/signup", AuthController.createUser);
-router.get("/verify", AuthController.verifyUser);
-router.post("/login", AuthController.authenticateUser);
-router.get("/saveProducts/",checktoken,AuthController.getUserProducts);
-router.post("/saveProducts/",checktoken,AuthController.addToProductList);
-router.get("/getuserproducts/:id",checktoken,AuthController.getUserProducts);
-router.get("/getproducts",checktoken,ProductController.getProducts);
+router.post("/api/signup", AuthController.createUser);
+router.get("api/verify", AuthController.verifyUser);
+router.post("/api/login", AuthController.authenticateUser);
+router.get("/api/saveProducts/",checktoken,AuthController.getUserProducts);
+router.post("/api/saveProducts/",checktoken,AuthController.addToProductList);
+router.get("/api/getuserproducts/:id",checktoken,AuthController.getUserProducts);
+router.get("/api/getproducts",checktoken,ProductController.getProducts);
 export default router;
