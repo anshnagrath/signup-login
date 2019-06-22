364f6140ad38abe8e298f0213d798a0a4737ec2a
Ansh nagrath <34484528+anshnagrath@users.noreply.github.com>
2019-06-22 21:02:51 +0530

    Update routes.js
    changes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,7 +5,7 @@ import checktoken from '../middlewares/authenticate'
 const router = express.Router();
 router.get('/',function(req,res){res.send("welcome to api")})
 router.post("/api/signup", AuthController.createUser);
-router.get("api/verify", AuthController.verifyUser);
+router.get("/api/verify", AuthController.verifyUser);
 router.post("/api/login", AuthController.authenticateUser);
 router.get("/api/saveProducts/",checktoken,AuthController.getUserProducts);
 router.post("/api/saveProducts/",checktoken,AuthController.addToProductList);
